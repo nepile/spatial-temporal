c76ff65c7e5371a7fe0dd61492fd6c0213920493
yogadewa0 <yogadewa0@gmail.com>
2025-09-22 14:10:43 +0700

    closed #1 Pengembangan pertama kelas kursi tanpa uji coba
    Signed-off-by: yogadewa0 <yogadewa0@gmail.com>

--- src/main/kotlin/Kursi.kt
+++ src/main/kotlin/Kursi.kt
@@ -1,2 +1,6 @@
-class Kursi {
+open class Kursi() {
+    open var panjang: Int = 0
+    open var lebar: Int = 0
+    open var tinggi: Int = 0
+    open var tipe: Array<String> = arrayOf("kayu", "besi")
 }
